b4d61866dd7447579b10f966da6e8cc7877c815c
amyzdong <45601121+amyzdong@users.noreply.github.com>
2018-12-06 10:49:08 -0500

    Update IPDS project.py

--- IPDS project.py
+++ IPDS project.py
@@ -1,8 +1,20 @@
 from pandas.io.html import read_html
-
 import pickle
 
 refresh = False
 
 if refresh:
-    page = '
+    page = 'html = https://en.wikipedia.org/wiki/Farebox_recovery_ratio'
+    wikitables = read_html(page)
+    table = wikitables [1]
+    pickle.dump(table, open("wiki_table.pkl", "w"))
+else:
+    table = pickle.load(open("wiki_table.pkl", "r"))
+
+continent = table[0]
+country = table[1]
+system = table[2]
+rates = table[3]
+
+for rate in rates:
+    print(rate)
